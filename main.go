271b50abc02b263575813bd232846cf1f77d4e69
Surendhar <surendhar@ymail.com>
2019-08-29 21:57:51 +0530

    Cleanup Code
    Signed-off-by: Surendhar <surendhar@ymail.com>

--- main.go
+++ main.go
@@ -9,32 +9,28 @@ import (
 )
 
 func main() {
-	data := readFile("samples/arp.json")
-
 	var net cccl.CcclNet
-
+	data := readFile("samples/arp.json")
 	err := json.Unmarshal(data, &net)
-
 	if err != nil {
 		fmt.Println("Error decoding JSON", err)
 	}
 
-	fmt.Printf("%-20v %-20v %-20v\n", "Name", "IP Address", "Mac Address")
+	formatString := "%-20v %-15v %-v\n"
+	fmt.Printf(formatString, "Name", "IP Address", "Mac Address")
 	for _, entry := range net.Arps {
-		fmt.Printf("%-20v %-20v %-20v\n", entry.Name, entry.IpAddress, entry.MacAddress)
+		fmt.Printf(formatString, entry.Name, entry.IpAddress, entry.MacAddress)
 	}
 }
 
 // returns file content in byte array
 func readFile(path string) []byte {
 	file, err := os.Open(path)
-
 	if err != nil {
 		fmt.Println("Error Opening file", err)
 	}
 
 	content, err := ioutil.ReadAll(file)
-
 	if err != nil {
 		fmt.Println("Error Reading content:", err)
 	}
